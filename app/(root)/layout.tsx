7db813594693c3853c927d9ea71ced5ecdf79562
Mohamed Elazhary <mmalazhari@users.noreply.github.com>
2024-12-18 21:14:05 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
