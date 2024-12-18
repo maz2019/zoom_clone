f1df4c1c25ccd80df98de09bd0d1e729f6f2728d
Mohamed Elazhary <mmalazhari@users.noreply.github.com>
2024-12-18 21:44:52 +0200

    2nd Try Deployment

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,29 +1,30 @@
 "use client";
-import React from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import {
   Sheet,
   SheetClose,
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import Image from "next/image";
-import Sidebar from "./Sidebar";
-import Link from "next/link";
 import { sidebarLinks } from "@/constants";
-import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 const MobileNav = () => {
   const pathname = usePathname();
+
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
         <SheetTrigger asChild>
           <Image
             src="/icons/hamburger.svg"
-            alt=""
             width={36}
             height={36}
+            alt="hamburger icon"
             className="cursor-pointer sm:hidden"
           />
         </SheetTrigger>
@@ -31,35 +32,37 @@ const MobileNav = () => {
           <Link href="/" className="flex items-center gap-1">
             <Image
               src="/icons/logo.svg"
-              alt="Yoom logo"
               width={32}
               height={32}
-              className="max-sm:size-10"
+              alt="yoom logo"
             />
-            <p className="text-[26px] text-white">Yoom</p>
+            <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="flex flex-col gap-5 pt-16 text-white">
-                {sidebarLinks.map((link) => {
-                  const isActive = pathname === link.route;
+              <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
+
                   return (
-                    <SheetClose asChild key={link.label}>
+                    <SheetClose asChild key={item.route}>
                       <Link
-                        href={link.route}
-                        key={link.label}
+                        href={item.route}
+                        key={item.label}
                         className={cn(
                           "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
-                          { "bg-blue-1": isActive }
+                          {
+                            "bg-blue-1": isActive,
+                          }
                         )}
                       >
                         <Image
-                          src={link.imgURL}
-                          alt={link.label}
+                          src={item.imgURL}
+                          alt={item.label}
                           width={20}
                           height={20}
                         />
-                        <p className="font-semibold ">{link.label}</p>
+                        <p className="font-semibold">{item.label}</p>
                       </Link>
                     </SheetClose>
                   );
